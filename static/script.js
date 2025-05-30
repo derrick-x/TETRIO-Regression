6f0ee0f9af080bb5bcf951f9e3d395127211cf81
Derrick <derrickx101@gmail.com>
2025-05-29 22:24:01 -0700

    work

--- static/script.js
+++ static/script.js
@@ -13,44 +13,44 @@ async function statsToGlicko() {
     document.getElementById("result1").textContent = data["result"]
 }
 async function estimateGlicko() {
-    let player = parseFloat(document.getElementById("player2").value);
+    let player = document.getElementById("player2").value;
     const res = await fetch("/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({function: "estimateGlicko", player: player})
+        body: JSON.stringify({function: "estimateGlicko", player: player.toLowerCase()})
     });
     const data = await res.json();
-    document.getElementById("result").textContent = data["result"]
+    document.getElementById("result2").textContent = data["result"]
 }
 async function getPlaystyle() {
-    let player = parseFloat(document.getElementById("player3").value);
+    let player = document.getElementById("player3").value.toLowerCase();
     const res = await fetch("/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({function: "getPlaystyle", player: player})
+        body: JSON.stringify({function: "getPlaystyle", player: player.toLowerCase()})
     });
     const data = await res.json();
     document.getElementById("result3").textContent = data["result"]
 }
 async function getMatchupPlayers() {
-    let player = parseFloat(document.getElementById("player4").value);
-    let opponent = parseFloat(document.getElementById("opponent4").value);
+    let player = document.getElementById("player4").value;
+    let opponent = document.getElementById("opponent4").value;
     const res = await fetch("/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({function: "getMatchupPlayers", player: player, opponent: opponent})
+        body: JSON.stringify({function: "getMatchupPlayers", player: player.toLowerCase(), opponent: opponent.toLowerCase()})
     });
     const data = await res.json();
     document.getElementById("result4").textContent = data["result"]
 }
 async function getMatchupStats() {
-    let player = parseFloat(document.getElementById("player5").value);
+    let player = document.getElementById("player5").value;
     let apm = parseFloat(document.getElementById("apm5").value);
     let pps = parseFloat(document.getElementById("pps5").value);
     let vs = parseFloat(document.getElementById("vs5").value);
@@ -59,19 +59,19 @@ async function getMatchupStats() {
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({function: "getMatchupStats", player: player, apm: apm, pps: pps, vs: vs})
+        body: JSON.stringify({function: "getMatchupStats", player: player.toLowerCase(), apm: apm, pps: pps, vs: vs})
     });
     const data = await res.json();
     document.getElementById("result5").textContent = data["result"]
 }
 async function getOpenerCoefficient() {
-    let player = parseFloat(document.getElementById("player6").value);
+    let player = document.getElementById("player6").value;
     const res = await fetch("/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({function: "getOpenerCoefficient", player: player})
+        body: JSON.stringify({function: "getOpenerCoefficient", player: player.toLowerCase()})
     });
     const data = await res.json();
     document.getElementById("result6").textContent = data["result"]
